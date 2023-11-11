afbdaa67aa487b0ed721b1dcf444a09d20714aa2
David Robarts <368796+robartsd@users.noreply.github.com>
2023-11-11 14:48:13 -0800

    track ETV, description, and review status changes

--- vineActivityInfo.user.js
+++ vineActivityInfo.user.js
@@ -5,10 +5,15 @@
 // @match       https://www.amazon.com/vine/orders
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @version     0.1
+// @version     0.2
 // ==/UserScript==
 
+const regex_orderID = /(?<=orderID=)[^&]+/;
+
 var statusTimestamp = Date.now();
+
+var orderIndex = GM_getValue("#orderIndex", {});
+var timestampIndex = GM_getValue("#timestampIndex",{});
 var completedReview = /review-type=completed/.test(location.search);
 
 document.querySelectorAll(".vvp-reviews-table--row, .vvp-orders-table--row").forEach((itemRow)=>{
@@ -23,27 +28,61 @@ document.querySelectorAll(".vvp-reviews-table--row, .vvp-orders-table--row").for
   var ASIN = itemLinkNode? itemLinkNode.href.split("/").pop() : itemTitleNode.textContent.split(" ")[0].trim();
   var item = GM_getValue(ASIN, {"ASIN":ASIN});
   if(itemLinkNode) {
-    Object.assign(item, {itemLink: itemLinkNode.href, itemTitle: itemTitleNode.querySelector(".a-truncate-full").textContent});
+    var itemTitle = itemTitleNode.querySelector(".a-truncate-full").textContent;
+    var itemTitleHistory = item.itemTitleHistory ? item.itemTitleHistory : {};
+    if(itemTitle != item.itemTitle || !Object.values(itemTitleHistory).includes(itemTitle)) {
+      itemTitleHistory[statusTimestamp] = itemTitle;
+    }
+    Object.assign(item, {itemLink: itemLinkNode.href, itemTitle, itemTitleHistory});
   }
   if(itemImageNode) {
     Object.assign(item, {itemImage: itemImageNode.src});
   }
   if(itemOrderNode) {
     Object.assign(item, {orderTimestamp: parseInt(itemOrderNode.dataset.orderTimestamp)});
+    timestampIndex[itemOrderNode.dataset.orderTimestamp] = ASIN;
   }
   if(itemETVNode) {
-    Object.assign(item, {orderETV: itemETVNode.textContent});
+    var orderETV = itemETVNode.textContent;
+    var orderEtvHistory = item.orderEtvHistory ? item.orderEtvHistory : {};
+    if(orderETV != item.orderETV || !Object.values(orderEtvHistory).includes(orderETV)) {
+      orderEtvHistory[statusTimestamp] = orderETV;
+    }
+    Object.assign(item, {orderETV, orderEtvHistory});
   }
   if(itemOrderLinkNode) {
-    Object.assign(item, {orderDetailsLink: itemOrderLinkNode.href});
+    var orderDetailsLink = itemOrderLinkNode.href;
+    var orderID = orderDetailsLink.match(regex_orderID)[0];
+    Object.assign(item, {orderDetailsLink, orderID});
+    orderIndex[orderID] = ASIN;
   }
   if(itemReviewNode) {
-    Object.assign(item, {reviewStatus: itemReviewNode.textContent, reviewTitle: itemReviewNode.dataset.reviewContent, reviewStatusTimestamp: statusTimestamp});
+    var reviewStatus = itemReviewNode.textContent;
+    var reviewStatusHistory = item.reviewStatusHistory ? item.reviewStatusHistory : {};
+    if(reviewStatus != item.reviewStatus) {
+      reviewStatusHistory[statusTimestamp] = reviewStatus;
+    }
+    if(item.reviewStatus && !Object.values(reviewStatusHistory).includes(item.reviewStatus)) {
+      reviewStatusHistory[item.reviewStatusTimestamp] = item.reviewStatus;
+    }
+    Object.assign(item, {reviewStatus, reviewStatusHistory, reviewTitle: itemReviewNode.dataset.reviewContent});
+    delete item.reviewStatusTimestamp;
   } else if(completedReview) {
-    Object.assign(item, {reviewStatus: "Item listing removed after review completed", reviewStatusTimestamp: statusTimestamp});
+    var reviewStatus = "Item listing removed after review completed";
+    var reviewStatusHistory = item.reviewStatusHistory ? item.reviewStatusHistory : {};
+    if(reviewStatus != item.reviewStatus) {
+      reviewStatusHistory[statusTimestamp] = reviewStatus;
+    }
+    if(item.reviewStatus && !Object.values(reviewStatusHistory).includes(item.reviewStatus)) {
+      reviewStatusHistory[item.reviewStatusTimestamp] = item.reviewStatus;
+    }
+    Object.assign(item, {reviewStatus, reviewStatusHistory});
+    delete item.reviewStatusTimestamp;
   }
   if(itemReviewLinkNode) {
     Object.assign(item, {reviewLink: itemReviewLinkNode.href})
   }
   GM_setValue(ASIN, item);
 });
+GM_setValue("#timestampIndex", timestampIndex);
+GM_setValue("#orderIndex", orderIndex);
