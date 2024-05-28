29455f9d232234f382e66b2c4bffc7dabf61920c
David Robarts <368796+robartsd@users.noreply.github.com>
2024-05-27 17:34:16 -0700

    avoid division by zero

--- evaluationMetrics.user.js
+++ evaluationMetrics.user.js
@@ -13,33 +13,37 @@ const percentBar = document.querySelector("#vvp-perc-reviewed-metric-display .an
 
 const count = parseInt(document.querySelector("#vvp-num-reviewed-metric-display strong").innerText);
 const percent = Math.round(parseFloat(percentText.querySelector("strong").innerText));
-const orderEstimate = Math.round(count/percent * 100);
 
-percentText.innerHTML = `You have reviewed <strong>${percent}%<\strong> of approximately ${orderEstimate} Vine items this period`;
+if (percent > 0) {
+  const orderEstimate = Math.round(count/percent * 100);
 
-const awaitingEstimate = orderEstimate - count;
-const periodStart = new Date(parseInt(document.querySelector("#vvp-eval-start-stamp").innerText));
-const periodEnd = new Date(parseInt(document.querySelector("#vvp-eval-end-stamp").innerText));
+  percentText.innerHTML = `You have reviewed <strong>${percent}%<\strong> of approximately ${orderEstimate} Vine items this period`;
 
-const periodFraction = ((new Date()).setUTCHours(0,0,0,0) - periodStart) / (periodEnd - periodStart);
-const projectedCount = count / periodFraction;
-const projectedOrders = orderEstimate / periodFraction;
-const projectedPercent = (projectedOrders - awaitingEstimate) / projectedOrders;
+  const awaitingEstimate = orderEstimate - count;
+  const periodStart = new Date(parseInt(document.querySelector("#vvp-eval-start-stamp").innerText));
+  const periodEnd = new Date(parseInt(document.querySelector("#vvp-eval-end-stamp").innerText));
 
-if (projectedCount < 70) {
-  countBar.style.backgroundColor = "red";
-} else if (projectedCount < 77) {
-  countBar.style.backgroundColor = "orange";
-} else if (projectedCount < 80) {
-  countBar.style.backgroundColor = "yellow";
+  const periodFraction = ((new Date()).setUTCHours(0,0,0,0) - periodStart) / (periodEnd - periodStart);
+  if (periodFraction > 0) {
+    const projectedCount = count / periodFraction;
+    const projectedOrders = orderEstimate / periodFraction;
+    const projectedPercent = (projectedOrders - awaitingEstimate) / projectedOrders;
+
+    if (projectedCount < 70) {
+      countBar.style.backgroundColor = "red";
+    } else if (projectedCount < 77) {
+      countBar.style.backgroundColor = "orange";
+    } else if (projectedCount < 80) {
+      countBar.style.backgroundColor = "yellow";
+    }
+
+    if (projectedPercent < .8) {
+      percentBar.style.backgroundColor = "red";
+    } else if (projectedPercent < .88) {
+      percentBar.style.backgroundColor = "orange";
+    } else if (projectedPercent < .92) {
+      percentBar.style.backgroundColor = "yellow";
+    }
+  }
 }
-
-if (projectedPercent < .8) {
-  percentBar.style.backgroundColor = "red";
-} else if (projectedPercent < .88) {
-  percentBar.style.backgroundColor = "orange";
-} else if (projectedPercent < .92) {
-  percentBar.style.backgroundColor = "yellow";
-}
-
 document.querySelector("#vvp-evaluation-period-tooltip-trigger").innerText = `Evaluation period: ${periodStart.toLocaleDateString()} - ${periodEnd.toLocaleString()}`;
