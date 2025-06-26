b8dc99c38e6f17fc251b2abefd217455972a3dcd
David Robarts <368796+robartsd@users.noreply.github.com>
2025-06-25 17:20:16 -0700

    Update evaluationMetrics.user.js for new account page

--- evaluationMetrics.user.js
+++ evaluationMetrics.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.amazon.com/vine/account*
 // @grant       none
-// @version     1.3.0
+// @version     1.4.0
 // @description Calculates approximate order total, displays evaluation period end time, and colors the activity bars if you are behind target
 // ==/UserScript==
 
@@ -11,11 +11,11 @@ const periodEnd = new Date(parseInt(document.querySelector("#vvp-eval-end-stamp"
 
 document.querySelector("#vvp-evaluation-date-string > strong").innerText = periodEnd.toLocaleString();
 
-const percent = Math.round(parseFloat(document.querySelector("#vvp-perc-reviewed-metric-display strong").innerText)); //Vine seems to always round to whole percent but sometimes displays inaccurate tenths
+const percent = Math.round(parseFloat(document.querySelector("#vvp-perc-reviewed-metric-display p:nth-child(-n+2) *:is(strong, .a-size-extra-large)").innerText)); //Vine seems to always round to whole percent but sometimes displays inaccurate tenths
 if (percent > 0) {
-  const metricsBox = document.querySelector("#vvp-vine-activity-metrics-box");
+  const metricsBox = document.querySelector("#vvp-vine-activity-metrics-box, #vvp-vine-account-details-box");
   metricsBox.dataset.percent = percent;
-  const count = parseInt(document.querySelector("#vvp-num-reviewed-metric-display strong").innerText.replaceAll(/\D+/g,'')); //review count should alwasy be integer remove possible thousands separator
+  const count = parseInt(document.querySelector("#vvp-num-reviewed-metric-display p:nth-child(-n+2) *:is(strong, .a-size-extra-large)").innerText.replaceAll(/\D+/g,'')); //review count should alwasy be integer remove possible thousands separator
   metricsBox.dataset.count = count;
   const orderCount = Math.round(count/percent * 100);
   metricsBox.dataset.orderCountEstimate = orderCount;
@@ -36,13 +36,13 @@ if (percent > 0) {
   const awaitingEstimate = awaitingMin == awaitingMax ? awaitingMax : `${awaitingMin}&ndash;${awaitingMax}`;
   const targetRequired = targetMin == targetMax ? targetMax : `${targetMin}&ndash;${targetMax}`;
   if (awaitingMax > 0) {
-    document.querySelector("#vvp-num-reviewed-metric-display p").innerHTML = `<strong>${count}</strong> reviews approved; ${awaitingEstimate} delivered items await review/approval`
+    document.querySelector("#vvp-num-reviewed-metric-display p:nth-child(-n+2):has(strong, .a-size-extra-large").innerHTML = `<span class="a-size-extra-large">${count} </span> ${awaitingEstimate} items await review`
   }
 
   if (targetMax > 0) {
-    document.querySelector("#vvp-perc-reviewed-metric-display p").innerHTML = `Reviews approved for <strong>${percent}%</strong> of ${orderEstimate} delivered items; ${targetRequired} more required to reach 90%`;
+    document.querySelector("#vvp-perc-reviewed-metric-display p:nth-child(-n+2):has(strong, .a-size-extra-large").innerHTML = `<span class="a-size-extra-large">${percent}%</span> of ${orderEstimate} items; ${targetRequired} more required to reach 90%`;
   } else {
-    document.querySelector("#vvp-perc-reviewed-metric-display p").innerHTML = `Reviews approved for <strong>${percent}%</strong> of ${orderEstimate} delivered items`;
+    document.querySelector("#vvp-perc-reviewed-metric-display p:nth-child(-n+2):has(strong, .a-size-extra-large").innerHTML = `<span class="a-size-extra-large">${percent}%</span> of ${orderEstimate} items`;
   }
 
   const periodStart = new Date(parseInt(document.querySelector("#vvp-eval-start-stamp").innerText));
@@ -54,8 +54,8 @@ if (percent > 0) {
     metricsBox.dataset.projectedOrderCount = projectedOrders.toFixed(1);
     const projectedPercent = (projectedOrders - awaitingMax) / projectedOrders;
     metricsBox.dataset.projectedReviewPercent = (projectedPercent * 100).toFixed(1);
-    const countBar = document.querySelector("#vvp-num-reviewed-metric-display .animated-progress span");
-    const percentBar = document.querySelector("#vvp-perc-reviewed-metric-display .animated-progress span");
+    const countBar = document.querySelector("#vvp-num-reviewed-metric-display [class*='animated-progress'] span");
+    const percentBar = document.querySelector("#vvp-perc-reviewed-metric-display [class*='animated-progress'] span");
 
     if (projectedCount < 70) {
       countBar.style.backgroundColor = "red";
